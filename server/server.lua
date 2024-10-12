eaef48004e8fc33d960b37b4dc1099149f66f144
Marolli <180851964+MarolliScripts@users.noreply.github.com>
2024-10-13 01:01:26 +0200

    Create server.lua

---
+++ server/server.lua
@@ -0,0 +1,82 @@
+local playerCooldowns = {}
+local cfg = Config.ShopRob
+
+ESX.RegisterServerCallback('DP_shoprobbery:firstCheck', function(source, cb)
+    local players = ESX.GetPlayers()
+    local webhook = cfg.Webhook
+    local licznikpolice = 0 
+    local currentTime = os.time() 
+    local cooldown = cfg.Cooldown
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    for i = 1, #players do 
+        local player = ESX.GetPlayerFromId(players[i])
+        if player and (player.job.name == cfg.Police or player.job.name == cfg.Sheriff) then
+            licznikpolice = licznikpolice + 1
+        end
+    end 
+
+    if licznikpolice < cfg.Potrzebni then 
+        return cb('brakpolicji') 
+    end 
+
+    if playerCooldowns[xPlayer.identifier] and (currentTime - playerCooldowns[xPlayer.identifier].lastRobbed) < cooldown then
+        return cb('cooldown') 
+    end 
+
+    cb(licznikpolice >= 1)
+    sendLog(webhook, 66666, 'DP_shoprobbery', GetPlayerName(source)..' rozpoczął napad na kasetkę!')
+end)
+
+ESX.RegisterServerCallback('DP_shoprobbery:getReward', function(source, cb)
+    local webhook = cfg.Webhook
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if not xPlayer then return end 
+
+    local reward = cfg.Rewards 
+    local currentTime = os.time() 
+    playerCooldowns[xPlayer.identifier] = {
+        lastRobbed = currentTime
+    }
+
+    local playerCoords = GetEntityCoords(GetPlayerPed(source))
+    local targetRadius = 6.0
+    local withinTarget = false
+
+    for i = 1, #Config.Sejfy do
+        local targetCoords = Config.Sejfy[i]
+        local distance = #(playerCoords - targetCoords)
+
+        if distance <= targetRadius then
+            withinTarget = true
+            break
+        end
+    end
+
+    if not withinTarget then
+        DropPlayer(source, 'Złapałem cię cheaterze!')
+        sendLog(webhook, 0xFF0000, 'DP_shoprobbery', GetPlayerName(source) .. ' Próbował zniszczyć system i nielegalnie wykraść pieniądze! Zbanuj go!')
+        return
+    end
+
+    xPlayer.addMoney(reward)
+    cb(reward)
+    sendLog(webhook, 66666, 'DP_shoprobbery', GetPlayerName(source) .. ' zgarnął z napadu na kasetkę **' .. reward .. '$!**')
+end)
+
+
+function sendLog(webhook, color, name, message)
+    local currentDate = os.date("%Y-%m-%d")
+    local currentTime = os.date("%H:%M:%S")
+    local embed = {
+          {
+              ["color"] = color,
+              ["title"] = "**".. name .."**",
+              ["description"] = message,
+              ["footer"] = {
+                  ["text"] = currentTime.." "..currentDate,
+              },
+          }
+      }
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+end
